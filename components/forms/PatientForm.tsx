ad15d6454b3a1a5e23e77670891dc37e886b1a9f
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-12-24 12:57:21 +0600

    edited color

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import CustomFormField from "../ui/CustomFormField";
 import SubmitButton from "../ui/SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/app/lib/validation";
+import { useRouter } from "next/navigation";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -21,6 +22,7 @@ export enum FormFieldType {
 }
 
 const PatientForm = () => {
+  const router = useRouter()
   const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
@@ -34,10 +36,27 @@ const PatientForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+    //   const userData = { name, email, phone };
+
+
+    //  const user =  await createUser(userData)
+
+    //  if(user) {
+    //       router.push(`/patients/${user.$id}/register`)
+    //  }
+
+
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <Form {...form}>
@@ -85,3 +104,7 @@ const PatientForm = () => {
 };
 
 export default PatientForm;
+
+
+
+// 59:22 - starting appwrite for backend
